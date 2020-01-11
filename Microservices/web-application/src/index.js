8b8849843fa8568c6467ab459e71074ee2b025e4
denypatrascu <denypatrascu@gmail.com>
2020-01-12 00:50:36 +0200

    Web Application

--- Microservices/web-application/src/index.js
+++ Microservices/web-application/src/index.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const expressSession = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
 const dotEnv = require('dotenv');
 
 dotEnv.config();
 
-const router = require(`${__dirname}/routes`);
-
 const app = express();
+
+const router = require(`${__dirname}/routes`);
 
 app.set('view engine', 'ejs')
    .set('views', `${__dirname}/views`)
    .set('layout', 'layout');
 
 app.use(express.static(`${__dirname}/public`))
+   .use(express.json())
    .use(expressLayouts)
+   .use(expressSession({
+        name: process.env.SESS_NAME,
+        secret: process.env.SESS_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false }
+   }))
    .use('/', router);
 
 const port = process.env.PORT;
--- Microservices/web-application/src/routes/index.js
+++ Microservices/web-application/src/routes/index.js
@@ -1,6 +1,53 @@
 const router = require('express').Router();
+const request = require('request');
+
+const WA = `[${process.env.SERVICE_NAME}]`,
+      // [WA]
+      REDIRECT_URI = `${process.env.HOSTNAME}:${process.env.PORT}`,
+      // http://localhost:8080
+      REQUEST_URI = `${process.env.SAS_HOSTNAME}:${process.env.SAS_PORT + process.env.SAS_REST_PATH}`;
+      // http://localhost:3000/api/sas
 
 router.get('/', (req, res) => {
+    res.render('home');
+});
+
+router.get('/login/spotify', (req, res) => {
+    const redirectUri = REDIRECT_URI;
+    // http://localhost:8080
+
+    const requestUri = `${REQUEST_URI}/spotify/login`;
+    // http://localhost:3000/api/sas/spotify/login
+
+    request.post(requestUri, {
+        json: { redirect: redirectUri }
+    }, (error, response, _) => {
+        if (error) {
+            console.error(`${WA} [ERROR]`);
+            console.error(error);
+            throw Error('#1');
+        }
+        res.redirect(response.request.uri.href);
+    });
+});
+
+router.post('/', (req, res) => {
+    req.session.token = req.body.token;
+    req.session.save((error) => {
+        if (error) {
+            console.error(`${WA} [ERROR]`);
+            console.error(error);
+            throw Error('#2');
+        }
+    });
+    console.log(req.session.token);
+    console.log(req.sessionID);
+    res.render('home');
+});
+
+router.get('/token', (req, res) => {
+    console.log(req.session.token);
+    console.log(req.sessionID);
     res.render('home');
 });
 
