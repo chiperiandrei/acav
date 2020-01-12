5d455ce06b9830dc5bfb91d8666a8638d3852bdf
denypatrascu <denypatrascu@gmail.com>
2020-01-12 16:00:43 +0200

    User Management Service

--- Microservices/user-management/usm.js
+++ Microservices/user-management/usm.js
@@ -4,7 +4,8 @@ const mysql = require('mysql')
 const app = express();
 const path = require('path');
 const router = express.Router();
-const apipath = '/api/usm';
+const apipath = '/api/ums';
+const request = require('request');
 var bodyParser = require('body-parser');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -40,12 +41,23 @@ router.post(apipath + '/register', (req, res) => {
     });
 });
 router.post(apipath + '/login', (req, res) => {
+    console.log(req.body);
     const email = req.body.email;
     const password = req.body.password;
 
     connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
-        if (results.length == 1) {
-            res.send("Logat!!")
+        if (results.length === 1) {
+            // res.send("Logat!!")
+            console.log('logat');
+
+            request.post('http://localhost:8080/login', {
+                json: { token: 1 }
+            }, (error, response, body) => {
+                if (error) {
+                    console.log(error);
+                    new Error();
+                }
+            });
         } else {
             res.send('email/ pass incorect');
         }
@@ -53,6 +65,9 @@ router.post(apipath + '/login', (req, res) => {
     });
 });
 app.use('/', router);
-app.listen(process.env.port || 4000);
 
-console.log('Running at Port 3000');
+const port = process.env.port || 4000;
+
+app.listen(port);
+
+console.log(`Running at Port ${port}`);
