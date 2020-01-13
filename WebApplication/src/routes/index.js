a0a989ac8b335b6b2ef8693b3c7db5a75f0cc427
denypatrascu <denypatrascu@gmail.com>
2020-01-13 22:21:32 +0200

    Web Application

--- WebApplication/src/routes/index.js
+++ WebApplication/src/routes/index.js
@@ -4,5 +4,6 @@ router.use('/', require('./home'));
 router.use('/login', require('./login'));
 router.use('/contact', require('./contact'));
 router.use('/connect', require('./connect'));
+router.use('/register', require('./register'));
 
 module.exports = router;
--- WebApplication/src/routes/login/index.js
+++ WebApplication/src/routes/login/index.js
@@ -11,34 +11,32 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    const redirectUri = `${env.WA.URI}/login`;
+    env.log('POST', `${env.WA.URI}/login`);
+
+    const redirectUri = `${env.WA.URI}/`;
     const requestUri = `${env.UMS.URI}/login`;
 
     const email = req.body.email;
     const password = req.body.password;
-
-    env.log('POST', redirectUri);
 
     if (email && password) {
         if (password.length >= 8) {
             request.post(requestUri, {
                 json: { email, password }
             }, (error, response, body) => {
-                if (error) {
-                    console.log(error);
-                }
-                env.log('POST', requestUri, { email, password }, false);
-                env.log('POST', requestUri, body, true);
+                if (!error && response.statusCode === 200) {
+                    env.log('POST', requestUri, { email, password }, false);
+                    env.log('POST', requestUri, body, true);
 
-                if (body && body.token) {
-                    req.session.user = {
-                        token: body.token,
-                        date: new Date()
-                    };
-                    res.redirect('/');
-                }
-                else {
-                    res.render('login', { message: body.message });
+                    if (body && body.token) {
+                        req.session.user = {
+                            token: body.token,
+                            date: new Date()
+                        };
+                        res.redirect(redirectUri);
+                    } else {
+                        res.render('login', {message: body.message});
+                    }
                 }
             });
         }
