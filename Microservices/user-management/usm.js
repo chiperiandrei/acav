0704d53b397340a456e8e5298e955d3efe22850f
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2020-01-14 21:03:55 +0200

    Update usm.js

--- Microservices/user-management/usm.js
+++ Microservices/user-management/usm.js
@@ -1,66 +1,100 @@
 const express = require('express');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt'); // TODO: Chiperi (add salt and encrypt, etc.)
-const dotEnv = require('dotenv');
-// const request = require('request');
-
-dotEnv.config();
-
-// TODO: add logs for /login, /register
-
+const mysql = require('mysql')
 const app = express();
 const router = express.Router();
-
 const REST_PATH = '/api/ums';
+var bodyParser = require('body-parser');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'acav'
+const dotEnv = require('dotenv');
+dotEnv.config();
+var connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERDB,
+    password: process.env.PASSWORDDB,
+    database: process.env.DATABASE
 });
+
+const bcrypt = require('bcrypt');
+const tokenGenerator = function (email, password) {
+    var token = '';
+    for (var i = 0; i < email.length; i++) {
+        token += email.charAt(i);
+        token += password.charAt(i);
+    }
+    return token;
+}
 
 connection.connect();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 router.post(REST_PATH + '/register', (req, res) => {
     const email = req.body.email;
-    const password = req.body.password;
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const token = tokenGenerator(email, password);
+    var sql = `INSERT INTO users (usertoken, email, password) VALUES ('${token}','${email}', '${password}')`;
+    connection.query(sql, function (err, result) {
+        if (err) {
+            res.status(404).send({
+                "error": "Something went wrong!"
+            });
 
-    const sql = `INSERT INTO users (email, password) VALUES ('${email}', '${password} ')`;
-    connection.query(sql, (error, result) => {
-        if (error) {
-            res.sendStatus(500);
-        } else {
-            res.sendStatus(200);
         }
+        res.status(200).send({
+            "success": "You have been registered!"
+        });
     });
 });
 
 router.post(REST_PATH + '/login', (req, res) => {
+
     const email = req.body.email;
     const password = req.body.password;
 
     connection.query(
-        'SELECT * FROM users WHERE email = ? AND password = ?',
-        [email, password], (error, results, fields) => {
-        if (results.length === 1) {
-            res.json({
-                token: 'test1234' // TODO: Chiperi
-            });
-        } else {
-            res.json({
-                message: 'Email or password is incorrect!'
-            });
+        'SELECT * FROM users WHERE email = ?',
+        [email], (error, results, fields) => {
+            if (results.length === 1) {
+                if (bcrypt.compareSync(password, results[0].password)) {
+                    res.json({
+                        token: results[0].usertoken
+                    });
+                } else {
+                    res.json({
+                        "message": "Password incorrect"
+                    });
+                }
+
+            } else {
+                res.status(404).json({
+                    message: 'Email or password is incorrect!'
+                });
+            }
+        });
+});
+router.post(REST_PATH + '/update-spotify-token', (req, res) => {
+    const spotifyToken = req.body.spotifytoken;
+    const usertoken = req.body.usertoken;
+    let sql = 'UPDATE users set spotifyToken=? WHERE usertoken=?';
+
+    let data = [spotifyToken, usertoken]
+    connection.query(sql, data, (error, results, fields) => {
+        if (results.affectedRows == 0) {
+            res.status(404).send({
+                "message": "Token incorrect!"
+            })
+        }
+        else {
+            res.status(200).send({
+                "message": "Token updated!"
+            })
         }
     });
-});
 
+});
 app.use('/', router);
 
-const port = process.env.PORT;
+const port = process.env.port || 4000;
 
 app.listen(port);
 
