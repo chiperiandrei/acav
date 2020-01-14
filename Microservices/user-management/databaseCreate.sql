750b38eacebbf9206a66be070e83043542a35b2a
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2020-01-14 21:16:09 +0200

    Update databaseCreate.sql

--- Microservices/user-management/databaseCreate.sql
+++ Microservices/user-management/databaseCreate.sql
@@ -1,26 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Gazdă: 127.0.0.1
+-- Timp de generare: ian. 14, 2020 la 08:08 PM
+-- Versiune server: 10.4.11-MariaDB
+-- Versiune PHP: 7.4.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-CREATE DATABASE IF NOT EXISTS `acav` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `acav`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Bază de date: acav
+--
+CREATE DATABASE IF NOT EXISTS acav DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE acav;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+-- --------------------------------------------------------
+
+--
+-- Structură tabel pentru tabel users
+--
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id int(11) NOT NULL,
+  usertoken varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  spotifyToken varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexuri pentru tabele eliminate
+--
 
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+--
+-- Indexuri pentru tabele users
+--
+ALTER TABLE users
+  ADD PRIMARY KEY (id),
+  ADD UNIQUE KEY email (email);
 
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pentru tabele eliminate
+--
+
+--
+-- AUTO_INCREMENT pentru tabele users
+--
+ALTER TABLE users
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+
+--
+-- Metadate
+--
+USE phpmyadmin;
+
+--
+-- Metadate pentru tabelul users
+--
+
+--
+-- Metadate pentru baza de date acav
+--
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
